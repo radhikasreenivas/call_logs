c7c85ec92f44418dbd7742bcbf9465b82e48ba3d
radhikasreenivas <radhikasreenivas16@gmail.com>
2015-06-04 17:51:49 +0530

    Initial commit

--- app/src/main/java/com/example/roadsider/saii/MainActivity.java
+++ app/src/main/java/com/example/roadsider/saii/MainActivity.java
@@ -1,58 +1,78 @@
 package com.example.roadsider.saii;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import static android.view.View.*;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import android.support.v7.app.ActionBarActivity;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    EditText phneno;
+    Button signup;
+    public static final String MyPREFERENCES = "MyPrefs";
+    public static final String Name = "nameKey";
+    SharedPreferences sharedpreferences;
+    SharedPreferences.Editor editor1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-          Button btn=(Button)findViewById(R.id.button);
-            final EditText et = (EditText)findViewById(R.id.editText);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                final String Name=et.getText().toString();
-            if(et.length()<10) {
-                et.requestFocus();
-                et.setError("Enter a valid ten digit number");
+        Boolean isFirstTime;
+        SharedPreferences app_preferences;
+        app_preferences = PreferenceManager
+                .getDefaultSharedPreferences(MainActivity.this);
+         editor1 = app_preferences.edit();
+        isFirstTime = app_preferences.getBoolean("isFirstTime", true);
+        if (isFirstTime) {
+            //implement your first time logic
+            phneno = (EditText) findViewById(R.id.editText);
+            signup = (Button) findViewById(R.id.button);
+            sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
-
-            }
-                else {
-                startActivity(new Intent(MainActivity.this, nextpage.class));
-            }
-            }
-        });
+            signup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String myphoneno = phneno.getText().toString();
+                    if (phneno.length() < 10) {
+                        phneno.requestFocus();
+                        phneno.setError("Enter a valid ten digit number");
+                    }
+                    else{
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putString(Name, myphoneno);
+                    editor.apply();
+                        editor1.putBoolean("isFirstTime", false);
+                        editor1.apply();
+                    startActivity(new Intent(MainActivity.this, nextpage.class));
+                    finish();}
+                }
+            });
+        } else {
+            //app open directly
+            startActivity(new Intent(MainActivity.this, nextpage.class));
+            finish();
+        }
     }
-
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
+
+
+
--- app/src/main/java/com/example/roadsider/saii/nextpage.java
+++ app/src/main/java/com/example/roadsider/saii/nextpage.java
@@ -1,62 +1,189 @@
 package com.example.roadsider.saii;
 
+
+
 import android.app.Activity;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
-import android.provider.CallLog;
-import android.support.v7.app.ActionBarActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.CallLog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.Date;
-
-
 public class nextpage extends Activity {
-
+    TextView callDetails;
+    ImageButton missed, outgng, incmng,all;
+    databasehelper dbhelper;
+    public Cursor mycursor;
+    public SQLiteDatabase db;
+    public StringBuilder sbb = new StringBuilder();
+    MainActivity m=new MainActivity();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nextpage);
-        Cursor mc=managedQuery(CallLog.Calls.CONTENT_URI,null,null,null,null);
-        int number=mc.getColumnIndex(CallLog.Calls.NUMBER);
-        int date=mc.getColumnIndex(CallLog.Calls.DATE);
-        int duration=mc.getColumnIndex(CallLog.Calls.DURATION);
-        int type=mc.getColumnIndex(CallLog.Calls.TYPE);
-        StringBuilder sb=new StringBuilder();
-        while(mc.moveToNext()){
-            String phnnum=mc.getString(number);
-            String calldur=mc.getString(duration);
-            String calldate=mc.getString(date);
-            String calltype=mc.getString(type);
-            Date d=new Date(date);
-            String callogstr="";
-            switch(Integer.parseInt(calltype))
-            {
-                case CallLog.Calls.OUTGOING_TYPE:
-                    callogstr="OUTGOING CALLS";
-                    break;
-                case CallLog.Calls.INCOMING_TYPE:
-                    callogstr="INCOMING CALLS";
-                    break;
-                case CallLog.Calls.MISSED_TYPE:
-                    callogstr="MISSED CALLS";
-                    break;
+        callDetails = (TextView) findViewById(R.id.callog);
+        all=(ImageButton) findViewById(R.id.imageButton);
+        outgng = (ImageButton) findViewById(R.id.imageButton3);
+        incmng = (ImageButton) findViewById(R.id.imageButton2);
+        missed = (ImageButton) findViewById(R.id.imageButton4);
+        addLog();
+        mycursor= getlog();
+        incode(mycursor);
+    }
 
-            }
-            sb.append(callogstr+"\n");
-            sb.append("Phone number"+phnnum+"\n");
-            sb.append("call duration"+calldur+"\n");
-            sb.append("call date"+d+"\n"+"\n"+"\n");
-            sb.append(System.getProperty("line.separator"));
+    public void help(View view)
+    {
+        ImageButton clicked=(ImageButton) view;
+
+        switch (clicked.getId()){
+            case R.id.imageButton:
+                 mycursor= getlog();
+                incode(mycursor);
+                break;
+            case R.id.imageButton2:
+                mycursor = incmng();
+                incode(mycursor);
+                break;
+            case R.id.imageButton3:
+                mycursor = outgng();
+                incode(mycursor);
+            break;
+            case R.id.imageButton4:
+                mycursor = missed();
+                incode(mycursor);
+                break;
+
         }
+    }
+    public Cursor incmng() {
+        dbhelper = new databasehelper(this);
+        db = dbhelper.getReadableDatabase();
+        String selectQuery = "SELECT * FROM calllogs WHERE TYPE=\"INCOMING CALLS\"";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        return cursor;
 
-        TextView callDetails=(TextView) findViewById(R.id.callog);
+    }
+
+    public Cursor missed() {
+        dbhelper = new databasehelper(this);
+        db = dbhelper.getReadableDatabase();
+        String selectQuery = "SELECT * FROM calllogs WHERE TYPE=\"MISSED CALLS\"";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        return cursor;
+    }
+
+    public Cursor outgng()
+    {
+        dbhelper = new databasehelper(this);
+        db = dbhelper.getReadableDatabase();
+        String selectQuery = "SELECT * FROM calllogs WHERE TYPE=\"OUTGOING CALLS\"";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        return cursor;
+    }
+
+
+
+
+    public void addLog() {
+
+        dbhelper = new databasehelper(this);
+        Cursor cursor;
+        ContentResolver cr = getContentResolver();
+        cursor = cr.query(
+                android.provider.CallLog.Calls.CONTENT_URI, null, null, null,
+                android.provider.CallLog.Calls.DATE + " DESC ");
+        db = dbhelper.getWritableDatabase();
+
+        db.delete(databasehelper.TABLE_NAME,null,null);
+        int idno=0;
+        int calltype = cursor.getColumnIndex(android.provider.CallLog.Calls.TYPE);
+        int numberColumnId = cursor.getColumnIndex(android.provider.CallLog.Calls.NUMBER);
+        int durationId = cursor.getColumnIndex(android.provider.CallLog.Calls.DURATION);
+        int dateId = cursor.getColumnIndex(android.provider.CallLog.Calls.DATE);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String cType = cursor.getString(calltype);
+                String callogstr = "";
+                switch (Integer.parseInt(cType)) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        callogstr = "OUTGOING CALLS";
+                        break;
+                    case CallLog.Calls.INCOMING_TYPE:
+                        callogstr = "INCOMING CALLS";
+                        break;
+
+                    case CallLog.Calls.MISSED_TYPE:
+                        callogstr = "MISSED CALLS";
+                        break;
+                }
+                String contactNumber = cursor.getString(numberColumnId);
+                String duration = cursor.getString(durationId);
+                String ddd=cursor.getString(dateId);
+                Date dd = new Date(Long.valueOf(ddd));
+                ContentValues values = new ContentValues();
+                values.put(databasehelper.ID,idno);
+                values.put(databasehelper.pnam, callogstr);
+                values.put(databasehelper.phno, contactNumber);
+                values.put(databasehelper.pdur, duration);
+                values.put(databasehelper.pdate, String.valueOf(dd));
+
+                db.insert(databasehelper.TABLE_NAME, null, values);
+                idno++;
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
+
+    public Cursor getlog() {
+        dbhelper = new databasehelper(this);
+        db = dbhelper.getReadableDatabase();
+        String selectQuery = "SELECT * FROM calllogs";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        return cursor;
+    }
+
+    public void incode(Cursor cursor)
+    {
+        StringBuilder sb=new StringBuilder();
+        int idno;
+        String type=null;
+        String phn=null;
+        String dur=null;
+        String dates=null;
+        if (cursor.moveToFirst()) {
+            do {
+                //  idno = cursor.getInt(0);
+                type = cursor.getString(1);
+                phn = cursor.getString(2);
+                dur = cursor.getString(3);
+                dates = cursor.getString(4);
+                sb.append(type).append("\n");
+                sb.append("Phone number : ").append(phn).append("\n");
+                sb.append("call duration : ").append(dur).append("\n").append("call date : ").append(dates).append("\n").append("\n").append("\n").append(System.getProperty("line.separator"));
+
+            } while (cursor.moveToNext());
+        }
         callDetails.setText(sb.toString());
+        cursor.close();
+    }
+
     }
 
 
 
 
 
-}
+
+
+
+
